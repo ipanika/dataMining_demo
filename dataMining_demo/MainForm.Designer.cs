f2c71a2b0c6d4a643e41bf9361bf4fe4eeab04ee
Ivan Gorbunov <ipanika91@gmail.com>
2013-10-22 11:54:40 +0400

    bugfix

--- dataMining_demo/MainForm.Designer.cs
+++ dataMining_demo/MainForm.Designer.cs
@@ -38,21 +38,25 @@
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 271);
+            this.button1.Location = new System.Drawing.Point(176, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(96, 37);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Run";
+            this.button1.Text = "Обработка объектов ИАД";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(175, 12);
+            this.button2.Location = new System.Drawing.Point(6, 23);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(97, 38);
             this.button2.TabIndex = 0;
@@ -62,7 +66,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(175, 56);
+            this.button3.Location = new System.Drawing.Point(6, 67);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(97, 38);
             this.button3.TabIndex = 2;
@@ -72,7 +76,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(175, 100);
+            this.button4.Location = new System.Drawing.Point(6, 111);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(96, 38);
             this.button4.TabIndex = 3;
@@ -83,7 +87,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Location = new System.Drawing.Point(4, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 4;
@@ -92,7 +96,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 29);
+            this.comboBox1.Location = new System.Drawing.Point(6, 40);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 5;
@@ -101,7 +105,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Location = new System.Drawing.Point(6, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 13);
             this.label2.TabIndex = 4;
@@ -110,7 +114,7 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(13, 73);
+            this.comboBox2.Location = new System.Drawing.Point(7, 84);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 5;
@@ -119,7 +123,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Location = new System.Drawing.Point(6, 111);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 4;
@@ -128,31 +132,55 @@
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(15, 117);
+            this.comboBox3.Location = new System.Drawing.Point(9, 128);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 153);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор объектов ИАД";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(169, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(112, 153);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Конструктор";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 306);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(296, 216);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.Text = "dataMining_demo";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -168,6 +196,8 @@
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
 
 
     }
--- dataMining_demo/MainForm.cs
+++ dataMining_demo/MainForm.cs
@@ -55,21 +55,15 @@ namespace dataMining_demo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            CreateDataAccessObjects(db);
-            MiningStructure ms = CreateMiningStructure(db);
-
-            CreateModels(ms);
-
             ProcessDatabase(db);
  
             db = svr.Databases["demo_DM"];
             db.Update(UpdateOptions.ExpandFull);
             SetModelPermissions(db, db.MiningStructures[0].MiningModels[0]);
             
-            //// Disconnect from the server
             svr.Disconnect();
 
-            MessageBox.Show("OK");           
+            MessageBox.Show("Анализ данных успешно завершен.");           
              
         }
 
@@ -97,178 +91,6 @@ namespace dataMining_demo
 
 
             return db;
-        }
-
-        void CreateDataAccessObjects(Database db)
-        {
-            // Create a relational data source
-            // by specifying the name and the id
-            RelationalDataSource ds = new RelationalDataSource("demo_ds", Utils.GetSyntacticallyValidID("demo_ds", typeof(Database)));
-            ds.ConnectionString = "Provider=SQLNCLI11.1;Data Source=localhost;Integrated Security=SSPI;Initial Catalog=demo_source";
-			
-            db.DataSources.Add(ds);
-          
-            // Create connection to datasource cto extract schema to a dataset
-            DataSet dset = new DataSet();
-            SqlConnection cn = new SqlConnection("Data Source=localhost; Initial Catalog=demo_source; Integrated Security=true");
-
-            // Create data adapters from database tables and load schemas
-            SqlDataAdapter daCustomers = new SqlDataAdapter("SELECT * FROM SourceData$", cn);
-            daCustomers.FillSchema(dset, SchemaType.Mapped, "SourceData$");
-
-            // Create the DSV, ad the dataset and add to the database
-            DataSourceView dsv = new DataSourceView("demo_dsv", "demo_dsv");
-            dsv.DataSourceID = "demo_ds";
-            dsv.Schema = dset.Clone();
-            db.DataSourceViews.Add(dsv);
-            db.DataSourceViews[0].ID = dsv.ID;
-
-            // Update the database to create the objects on the server
-            db.Update(UpdateOptions.ExpandFull);
-        }
-
-        MiningStructure CreateMiningStructure(Database db)
-        {
-            // Initialize a new mining structure
-            MiningStructure ms = new MiningStructure("demo_structure", "demo_structure");
-            ms.Source = new DataSourceViewBinding(db.DataSourceViews[0].ID);
-
-            // Create the columns of the mining structure 
-            // setting the type, content and data binding
-
-            //// User Id column
-            ScalarMiningStructureColumn col1 = new ScalarMiningStructureColumn("ID", "ID");
-            col1.Type = MiningStructureColumnTypes.Long;
-            col1.Content = MiningStructureColumnContents.Key;
-            col1.IsKey = true;
-            //// Add data binding to the column
-            col1.KeyColumns.Add("SourceData$", "ID", System.Data.OleDb.OleDbType.Integer);
-            //// Add the column to the mining structure
-            ms.Columns.Add(col1);
-
-            //// Generation column
-            ScalarMiningStructureColumn col2 = new ScalarMiningStructureColumn("Marital Status", "Marital Status");
-            col2.Type = MiningStructureColumnTypes.Text;
-            col2.Content = MiningStructureColumnContents.Discrete;
-            // Add data binding to the column
-            col2.KeyColumns.Add("SourceData$", "Marital Status", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col2);
-
-            ScalarMiningStructureColumn col3 = new ScalarMiningStructureColumn("Gender", "Gender");
-            col3.Type = MiningStructureColumnTypes.Text;
-            col3.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col3.KeyColumns.Add("SourceData$", "Gender", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col3);
-
-            ScalarMiningStructureColumn col4 = new ScalarMiningStructureColumn("Yearly Income", "Yearly Income");
-            col4.Type = MiningStructureColumnTypes.Long;
-            col4.Content = MiningStructureColumnContents.Discretized;
-            // col3 data binding to the column
-            col4.KeyColumns.Add("SourceData$", "Yearly Income", System.Data.OleDb.OleDbType.Integer);
-            // Add the column to the mining structure
-            ms.Columns.Add(col4);
-
-            ScalarMiningStructureColumn col5 = new ScalarMiningStructureColumn("Children", "Children");
-            col5.Type = MiningStructureColumnTypes.Long;
-            col5.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col5.KeyColumns.Add("SourceData$", "Children", System.Data.OleDb.OleDbType.Integer);
-            // Add the column to the mining structure
-            ms.Columns.Add(col5);
-
-            ScalarMiningStructureColumn col6 = new ScalarMiningStructureColumn("Education", "Education");
-            col6.Type = MiningStructureColumnTypes.Text;
-            col6.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col6.KeyColumns.Add("SourceData$", "Education", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col6);
-
-            ScalarMiningStructureColumn col7 = new ScalarMiningStructureColumn("Occupation", "Occupation");
-            col7.Type = MiningStructureColumnTypes.Text;
-            col7.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col7.KeyColumns.Add("SourceData$", "Occupation", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col7);
-
-            ScalarMiningStructureColumn col8 = new ScalarMiningStructureColumn("Home Owner", "Home Owner");
-            col8.Type = MiningStructureColumnTypes.Text;
-            col8.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col8.KeyColumns.Add("SourceData$", "Home Owner", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col8);
-
-            ScalarMiningStructureColumn col9 = new ScalarMiningStructureColumn("Cars", "Cars");
-            col9.Type = MiningStructureColumnTypes.Long;
-            col9.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col9.KeyColumns.Add("SourceData$", "Cars", System.Data.OleDb.OleDbType.Integer);
-            // Add the column to the mining structure
-            ms.Columns.Add(col9);
-
-            ScalarMiningStructureColumn col10 = new ScalarMiningStructureColumn("Commute Distance", "Commute Distance");
-            col10.Type = MiningStructureColumnTypes.Text;
-            col10.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col10.KeyColumns.Add("SourceData$", "Commute Distance", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col10);
-
-            ScalarMiningStructureColumn col11 = new ScalarMiningStructureColumn("Region", "Region");
-            col11.Type = MiningStructureColumnTypes.Text;
-            col11.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col11.KeyColumns.Add("SourceData$", "Region", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col11);
-
-            ScalarMiningStructureColumn col12 = new ScalarMiningStructureColumn("Age", "Age");
-            col12.Type = MiningStructureColumnTypes.Long;
-            col12.Content = MiningStructureColumnContents.Continuous;
-            // col3 data binding to the column
-            col12.KeyColumns.Add("SourceData$", "Age", System.Data.OleDb.OleDbType.Integer);
-            // Add the column to the mining structure
-            ms.Columns.Add(col12);
-
-            ScalarMiningStructureColumn col13 = new ScalarMiningStructureColumn("BikeBuyer", "BikeBuyer");
-            col13.Type = MiningStructureColumnTypes.Text;
-            col13.Content = MiningStructureColumnContents.Discrete;
-            // col3 data binding to the column
-            col13.KeyColumns.Add("SourceData$", "BikeBuyer", System.Data.OleDb.OleDbType.WChar);
-            // Add the column to the mining structure
-            ms.Columns.Add(col13);
-            
-            // Add the mining structure to the database
-            db.MiningStructures.Add(ms);
-            ms.Update();
-
-            return ms;
-
-        }
-        
-        void CreateModels(MiningStructure ms)
-        {
-            MiningModel ClusterModel;
-            //MiningModel TreeModel;
-          //  MiningModelColumn mmc;
-
-            // Create the Cluster model and set the algorithm 
-            // and parameters
-            ClusterModel = ms.CreateMiningModel(true, "BikeBuyer Clusters");
-            ClusterModel.Algorithm = "Microsoft_Clustering";
-            ClusterModel.AlgorithmParameters.Add("CLUSTER_COUNT", 0);
-            ClusterModel.AlgorithmParameters.Add("CLUSTERING_METHOD", 3);
-
-            // The CreateMiningModel method adds 
-            // all the structure columns to the collection
-
-            // Submit the models to the server
-            ClusterModel.Update();
         }
 
         void ProcessDatabase(Database db)
@@ -308,34 +130,39 @@ namespace dataMining_demo
         void SetModelPermissions(Database db, MiningModel mm)
         {
             // Create a new role and add members
-            Role r = new Role("ModelReader", "ModelReader");
 
-            String userName = SystemInformation.UserName;
-            String PCName = Environment.MachineName;
+            Role r = db.Roles.FindByName("ModelReader");
+            if (r == null)
+            {
+                r = new Role("ModelReader", "ModelReader");
 
-            //r.Members.Add(new RoleMember("user-ПК\\user"));
-            r.Members.Add(new RoleMember(PCName + "\\" + userName));
+                String userName = SystemInformation.UserName;
+                String PCName = Environment.MachineName;
 
-            // Add the role to the database and update
-            db.Roles.Add(r);
-            r.Update();
+                //r.Members.Add(new RoleMember("user-ПК\\user"));
+                r.Members.Add(new RoleMember(PCName + "\\" + userName));
 
+                // Add the role to the database and update
+                db.Roles.Add(r);
+                r.Update();
 
-            // Create a permission object referring the role
-            MiningModelPermission mmp = new MiningModelPermission();
-            mmp.Name = "ModelReader";
-            mmp.ID = "ModelReader";
-            mmp.RoleID = "ModelReader";
+                // Create a permission object referring the role
+                MiningModelPermission mmp = new MiningModelPermission();
+                mmp.Name = "ModelReader";
+                mmp.ID = "ModelReader";
+                mmp.RoleID = "ModelReader";
 
-            // Assign access rights to the permission
-            mmp.Read = ReadAccess.Allowed;
-            mmp.AllowBrowsing = true;
-            mmp.AllowDrillThrough = true;
-            mmp.ReadDefinition = ReadDefinitionAccess.Allowed;
+                // Assign access rights to the permission
+                mmp.Read = ReadAccess.Allowed;
+                mmp.AllowBrowsing = true;
+                mmp.AllowDrillThrough = true;
+                mmp.ReadDefinition = ReadDefinitionAccess.Allowed;
 
-            // Add permission to the model and update
-            mm.MiningModelPermissions.Add(mmp);
-            mmp.Update();
+                // Add permission to the model and update
+                mm.MiningModelPermissions.Add(mmp);
+                mmp.Update();
+            }
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
