aad164898160d5ea3fe754bc3f1168bc84800549
ivan gorbunov <ipanika91@gmail.com>
2013-11-14 22:14:41 +0400

    bugfix

--- dataMining_demo/FormMetaData.Designer.cs
+++ dataMining_demo/FormMetaData.Designer.cs
@@ -47,6 +47,7 @@
             this.Column2,
             this.Column3,
             this.Column4});
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(322, 247);
--- dataMining_demo/FormSelection.Designer.cs
+++ dataMining_demo/FormSelection.Designer.cs
@@ -46,6 +46,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
             this.dataGridView1.Location = new System.Drawing.Point(12, 144);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(403, 235);
